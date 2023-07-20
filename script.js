983dd1985b3355d5910b71bd672e6d9abafdafb4
khalidBouh <khaalidm26@gmail.com>
2023-07-20 15:31:50 +0300

    Done Exercise

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("hello");
+
+let grade = 61
+
+if (grade >= 90 ){
+    console.log("You got an A grade")
+} else if (grade >= 75  ){
+    console.log("You got an B grade")
+} else if (grade >= 60 ){
+    console.log("You got an C grade")
+} else{
+    console.log("You got an Failed")
+}
